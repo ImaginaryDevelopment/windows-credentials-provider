382a97df384c7aa0004a408c7f373e87c694b7e8
DimperioB <DimperioB@duvalschools.org>
2024-06-25 11:51:16 -0400

    migrate camera control into distinct component

--- CredentialHelper/Reusable.fs
+++ CredentialHelper/Reusable.fs
@@ -2,6 +2,11 @@
 
 open System
 
+type Property<'t> = {
+    Getter: unit -> 't
+    Setter: 't -> unit
+}
+
 let failNullOrEmpty paramName x = if String.IsNullOrEmpty x then raise <| ArgumentOutOfRangeException paramName
 
 let (|ValueString|WhiteSpace|NonValueString|) =
@@ -32,6 +37,11 @@ let replace delimiter replacement text =
 
 let afterOrSelf delimiter x = x |> tryAfter delimiter |> Option.defaultValue x
 
+let uncurry f (x,y) = f x y
+
+let inline defer f x =
+    fun () -> f x
+
 let (|After|_|) delimiter text =
     text |> tryAfter delimiter
 
@@ -39,3 +49,15 @@ module Tuple2 =
     let mapSnd f (x,y) =
         x, f y
     let withSnd y x = x,y
+
+let toDisposable (x: #System.IDisposable) =
+    x :> System.IDisposable
+
+let dispose (x:System.IDisposable) =
+    x.Dispose()
+
+let tryDispose title x =
+    try
+        dispose x
+    with _ ->
+        eprintfn "Failed to dispose: '%s'" title
