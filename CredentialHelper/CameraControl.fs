9efb715056a24fc9da4c05ef935cefcb42b7abfd
DimperioB <DimperioB@duvalschools.org>
2024-06-26 15:07:31 -0400

    refactor capture into wrapper, add camera state enum, fix button states to match camera state

--- CredentialHelper/CameraControl.fs
+++ CredentialHelper/CameraControl.fs
@@ -26,52 +26,107 @@ type OnCredentialSubmitHandler = delegate of System.Net.NetworkCredential -> uni
 type OnAction = delegate of unit -> unit
 
 
-type CameraControl(imageProp: Property<Image>) =
+type CameraState =
+    | Stopped
+    | Initializing
+    | Started
 
-    let mutable index = 0
+// isolate startup and shutdown
+type CaptureWrapper (index:int) =
 
     let mutable capture : VideoCapture = null
+    let tryGetCapture() =
+        match capture with
+        | null -> None
+        | _ when capture.IsDisposed -> None
+        | _ -> Some capture
+
+    member this.TryStart() =
+        this.CleanUp()
+        capture <- new VideoCapture(index)
+        capture.Open index |> ignore<bool>
+        capture.IsOpened()
+
+    member _.CleanUp () =
+        tryGetCapture()
+        |> Option.iter(fun _ ->
+            let inline tryF title f =
+                try
+                    f()
+                with ex ->
+                    eprintfn "capture %s error: %s" title ex.Message
+            // release will throw if already disposed
+            // https://github.com/shimat/opencvsharp/blob/728293a3631386af5edafccaa9ba31ceeec9e0a6/src/OpenCvSharp/Modules/videoio/VideoCapture.cs#L1112
+            tryF "release" (fun () -> capture.Release())
+            tryF "dispose" (fun () -> capture.Dispose())
+            capture <- null
+        )
+
+    member _.Read (frame:Mat) =
+        tryGetCapture()
+        |> Option.map(fun capture ->
+            capture.Read(frame)
+        )
+        |> Option.defaultValue false
+
+    member this.Dispose() = this.CleanUp()
+
+    interface IDisposable with
+        member this.Dispose() = this.Dispose()
+
+
+type CameraControl(imageProp: Property<Image>) =
+
+    let captureWrapper = new CaptureWrapper(0)
     // hold onto image so it can be disposed
     let mutable image: Bitmap = null
-    let mutable camera : Thread = null // Thread(ThreadStart(this.capturecameracallback))
+    let mutable cameraThread : Thread = null // Thread(ThreadStart(this.capturecameracallback))
+    let mutable cameraState = Stopped
 
-    let mutable isCameraRunning = false
+    member _.CameraState = cameraState
 
-
-    member _.CaptureCamera () =
-        // TODO: consider performance of running in too tight of a loop
-        let captureCameraCallback =
-            let mutable frame: Mat = null
-            fun () ->
-                frame <- new Mat()
-                capture <- new VideoCapture(index)
-                capture.Open index |> ignore<bool>
-                if capture.IsOpened() then
-                    isCameraRunning <- true
-                    while isCameraRunning do
-                        capture.Read frame |> ignore<bool>
-                        image <- BitmapConverter.ToBitmap frame
-                        imageProp.Getter()
-                        |> Option.ofObj
-                        |> Option.iter(fun image ->
-                            image.Dispose()
-                        )
-                        imageProp.Setter image
-                ()
-        match camera with
-        | null -> ()
-        | _ -> camera.Abort()
-        camera <- Thread(ThreadStart captureCameraCallback)
-        camera.Start()
+    member _.CaptureCamera (index: int) =
+        match cameraState with
+        | Started
+        | Initializing -> ()
+        | Stopped ->
+            // TODO: consider performance of running in too tight of a loop
+            let captureCameraCallback =
+                let mutable frame: Mat = null
+                fun () ->
+                    cameraState <- Initializing
+                    frame <- new Mat()
+                    if captureWrapper.TryStart() then
+                        while cameraState <> Stopped do
+                            if captureWrapper.Read frame then
+                                cameraState <- Started
+                                image <- BitmapConverter.ToBitmap frame
+                                imageProp.Getter()
+                                |> Option.ofObj
+                                |> Option.iter(fun image ->
+                                    image.Dispose()
+                                )
+                                imageProp.Setter image
+                            else
+                                eprintfn "Camera read failed"
+                    ()
+            match cameraThread with
+            | null -> ()
+            | _ -> cameraThread.Abort()
+            cameraThread <- Thread(ThreadStart captureCameraCallback)
+            cameraThread.Name <- "Camera reader"
+            cameraThread.Start()
 
     member _.StopCapture () =
-        if isCameraRunning then
-            isCameraRunning <- false
-            capture :> IDisposable |> _.Dispose()
-            capture |> Option.ofObj |> Option.iter _.Release()
+        match cameraState with
+        | Stopped -> ()
+        | _ ->
+            cameraState <- Stopped
+            captureWrapper.CleanUp()
 
     member _.TakeSnap () =
-        if isCameraRunning then
+        match cameraState with
+        | CameraState.Started ->
             match imageProp.Getter() with
             | null -> Error "image getter returned null"
             | image ->
@@ -82,23 +137,26 @@ type CameraControl(imageProp: Property<Image>) =
                     | :? ArgumentException as ex ->
                         Error ex.Message
 
-        else
+        | CameraState.Stopped ->
             let msg = "Cannot take picture if the camera isn't capturing images"
             printfn $"{msg}"
             Error msg
+        | CameraState.Initializing ->
+            Error "Camera is starting up"
 
     member _.Dispose() =
-        camera
+        cameraThread
         |> Option.ofObj
         |> Option.iter(fun _ ->
-            if camera.IsAlive then
+            cameraState <- Stopped
+            if cameraThread.IsAlive then
                 try
-                    camera.Abort()
+                    cameraThread.Abort()
                 with _ -> ()
         )
         let disposals : (string * IDisposable) list =
             [
-                nameof capture, capture // |> Option.ofObj |> Option.map toDisposable
+                nameof captureWrapper, captureWrapper // |> Option.ofObj |> Option.map toDisposable
                 nameof image, image // |> Option.ofObj |> Option.map toDisposable
             ]
         disposals
@@ -126,19 +184,37 @@ type Form1() as this =
             TabStop=false
         )
 
-    let imControl = new CameraControl({Getter=(fun() -> pictureBox1.Image); Setter= fun v -> pictureBox1.Image <- v})
+    let mutable onceInitialized = None
+    let imControl =
+        let mutable cc : CameraControl option = None
+        let setter v =
+            cc
+            |> function
+                | None -> eprintfn "Setter called without camera control"
+                | Some cc ->
+                    if cc.CameraState = CameraState.Started then
+                        onceInitialized
+                        |> Option.iter(fun f ->
+                            f()
+                        )
+            pictureBox1.Image <- v
+        let imControl = new CameraControl({Getter=(fun() -> pictureBox1.Image); Setter= setter})
+        cc <- Some imControl
+        imControl
+
     let qrControl = QRCode.QrManager()
 
     let generateDefaultPath () = System.String.Format(@"image-{0}.jpg", Guid.NewGuid())
 
     let button1 = new Button(
-        Location = new System.Drawing.Point(65, 478),
+        Location = new System.Drawing.Point(65, 491),
         Name = "button1",
         Size = new System.Drawing.Size(144, 52),
         TabIndex = 1,
-        Text = "Start",
+        Text = "Initializing",
         UseVisualStyleBackColor = true,
-        Visible = false
+        Visible = true,
+        Enabled = false
     )
 
     let button2 = new Button(
@@ -148,37 +224,55 @@ type Form1() as this =
        Size = new System.Drawing.Size(459, 86),
        TabIndex = 1,
        Text = "scan",
+       Enabled = false,
        UseVisualStyleBackColor = true
     )
 
     let button1Click _ _ =
-                if button1.Text = "Start" then
-                    imControl.CaptureCamera()
-                    button1.Text <- "Stop"
-                    button2.Text <- "scan"
-                else
-                    imControl.StopCapture()
-                    button1.Text <- "Start"
+        match imControl.CameraState with
+        | Initializing -> ()
+        | Stopped ->
+            imControl.CaptureCamera(0)
+            button1.Text <- "Stop"
+            button2.Text <- "scan"
+        | CameraState.Started ->
+            imControl.StopCapture()
+            button1.Text <- "Start"
 
     let button2Click _ _ =
-        imControl.TakeSnap()
-        |> function
-            | Error msg -> System.Windows.Forms.MessageBox.Show(msg) |> ignore
-            | Ok snapshot ->
-            //generateDefaultPath()
-            //|> snapshot.Save
-            qrControl.TryDecode(snapshot)
-            |> Option.iter(fun qrResult ->
-                System.Windows.Forms.MessageBox.Show(qrResult) |> ignore
-                //button2.Text <- qrResult
-            )
+        match imControl.CameraState with
+        | Initializing -> ()
+        | Stopped -> ()
+        | Started ->
+            imControl.TakeSnap()
+            |> function
+                | Error msg -> System.Windows.Forms.MessageBox.Show(msg) |> ignore
+                | Ok snapshot ->
+                //generateDefaultPath()
+                //|> snapshot.Save
+                qrControl.TryDecode(snapshot)
+                |> Option.iter(fun qrResult ->
+                    System.Windows.Forms.MessageBox.Show(qrResult) |> ignore
+                    //button2.Text <- qrResult
+                )
 
     let components : System.ComponentModel.IContainer = null
 
     do
         this.InitializeComponent()
-        imControl.CaptureCamera()
-        button1.Text <- "Stop"
+        // there is a startup time to grabbing the camera and starting to display it on the screen
+
+        // relies on capture camera invoking the setter above to kick off post-initializing work
+        onceInitialized <- Some <| fun _ ->
+            let updatebutton: OnAction =
+                OnAction(fun _ ->
+                    button1.Text <- "Stop"
+                    button1.Enabled <- imControl.CameraState <> CameraState.Initializing
+                    button2.Enabled <- imControl.CameraState = CameraState.Started
+                    )
+            button1.Invoke updatebutton |> ignore
+        imControl.CaptureCamera(0)
+
 
     override this.OnClosed e =
         base.OnClosed e
